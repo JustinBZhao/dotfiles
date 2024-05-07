3cbcf60e0cbdf3827153daf329a90b65c1b8cc2a
Justin Zhao <zhaoboqin58@gmail.com>
2024-05-07 01:14:32 -0700

    Improved install script

--- executable_install_packages.sh
+++ executable_install_packages.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# List of manually installed packages to be synchronized
+# Set up most of the packages and plugins
+
+# Reject execution if run as root
+if [ "$(id -u)" -eq 0 ]; then
+    echo "This script cannot be run as root! Abort."
+    exit 1
+fi
 
 PACKAGES=(
     build-essential
@@ -34,7 +40,7 @@ UBUNTU_ONLY_PACKAGES=(
 )
 
 # Installation command
-sudo apt update
+sudo apt update && sudo apt upgrade || { echo "Failed to update package lists!"; exit 1; }
 # First check if this is a WSL installation
 # Only install specific packages on full Ubuntu distribution
 echo "------------------------------------"
@@ -42,13 +48,18 @@ if [ -n "$WSL_DISTRO_NAME" ]; then
     echo "This is a WSL installation. Skipping some packages."
 else
     echo "This is a full installation. Install specific packages."
-    sudo apt install ${UBUNTU_ONLY_PACKAGES[@]}
+    sudo apt install "${UBUNTU_ONLY_PACKAGES[@]}"
 fi
 echo "------------------------------------"
 # Then install general packages
 echo "Installing general packages..."
-sudo apt install ${PACKAGES[@]}
-chsh -s $(which zsh) # it looks like setting default shell might not always succeed
+sudo apt install "${PACKAGES[@]}"
+# Change default shell to zsh
+if command -v chsh >/dev/null 2>&1; then
+    chsh -s $(which zsh) || { echo "Failed to change the default shell to zsh."; exit 1; } # it looks like setting default shell might not always succeed
+else
+    echo "chsh command not found, cannot change default shell to zsh!"
+fi
 echo "Package installation complete!"
 
 # Install Oh my Zsh
@@ -67,9 +78,10 @@ if [ ! -d "$ZSH_HIGHLIGHT_DIR" ]; then
 fi
 
 # Install vim-plug
-if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
+vim_plug_dir="$HOME/.vim/autoload/plug.vim"
+if [ ! -f "$vim_plug_dir" ]; then
     echo "Installing vim-plug..."
-    curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    curl -fLo "$vim_plug_dir" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     echo "vim-plug installation finished!"
     vim -c "PlugInstall" -c "qa" # execute 'PlugInstall' in Vim
